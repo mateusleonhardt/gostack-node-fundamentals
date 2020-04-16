99c073bfeeac364c5dd21a1d7c30f0a02633756b
Mateus leonhardt <mateus.leonhardt@gmail.com>
2020-04-16 10:55:31 -0300

    Finished challange

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,5 +1,11 @@
 import Transaction from '../models/Transaction';
 
+interface CreateTransaction {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 interface Balance {
   income: number;
   outcome: number;
@@ -14,15 +20,45 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.transactions.reduce(
+      (incomeAccumulator, transaction) => {
+        return transaction.type === 'income'
+          ? incomeAccumulator + transaction.value
+          : incomeAccumulator;
+      },
+      0,
+    );
+
+    const outcome = this.transactions.reduce(
+      (incomeAccumulator, transaction) => {
+        return transaction.type === 'outcome'
+          ? incomeAccumulator + transaction.value
+          : incomeAccumulator;
+      },
+      0,
+    );
+
+    const total = income - outcome;
+
+    const balance: Balance = {
+      income,
+      outcome,
+      total,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransaction): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
